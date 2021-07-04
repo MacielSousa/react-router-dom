4fbd67b6a635743e56a95ea78f8f565f3d712956
macielsousa <fmacielsousa@gmail.com>
2021-07-04 20:06:39 -0300

    adding helmet to home component

--- myapp/src/NestedRoutes/Home.js
+++ myapp/src/NestedRoutes/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Head from './Head'
+import {Helmet} from 'react-helmet';
 
 function Home() {
   return (
       <div>
-        <Head title="Home" description="Essa é a descrição da Home"/>
+       {/*<Head title="Home" description="Essa é a descrição da Home"/>*/}
+       <Helmet>
+         <title>Dogs | Home</title>
+         <meta name="description" content="Essa é a descrição da Home" />
+       </Helmet>
           <h1>Home</h1>
           <p>Essa é a Home</p>
           <Link to="produto/notebook">Notebook</Link> {'  '}
